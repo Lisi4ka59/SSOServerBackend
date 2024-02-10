efeaa67e9f857a741f9fe2897467c9c99e7d475a
Lisi4ka <0059nai@gmail.com>
2024-02-10 10:35:09 +0300

    initial commit

---
+++ src/main/java/com/lisi4ka/ssoserverbackend/SsoServerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lisi4ka.ssoserverbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoServerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoServerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lisi4ka/ssoserverbackend/SsoServerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisi4ka.ssoserverbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoServerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
